4648daf20dbaa3b41076a7618ba8ee2be58dc711
Harish Kilaru <hckilaru@gmail.com>
2016-07-25 19:40:42 -0400

    removed many console logs

--- public/components/rebindNode.jsx
+++ public/components/rebindNode.jsx
@@ -3,25 +3,16 @@
 var RebindNode = React.createClass({
   //this is the function that actually changes the binding of the key.
   updateKeyBinding: function(event) {
-    console.log("updateKeyBinding called");
     var code = this.props.targetKey.charCodeAt();
-
-    //this.props.targetSong  is going to be entire song object
-    // var path = "/soundfiles/" + this.props.targetSong;
-
     var path = this.props.targetSong.soundLink;
     if(path[0] === "/") {
       path = "." + path;
-      console.log("NEW PATH", path);
     }
     var songName = this.props.targetSong.name;
     var bindings = this.props.bindings;
-    console.log("PATH", path);
     var pathPrefix = path.split('').splice(0, 3).join('');
-    console.log("path prefix", pathPrefix);
     //if the rebind is for an uploaded sound
     if(pathPrefix === "./d") {
-      console.log("sound was uploaded!");
       $.ajax({
         type: 'POST',
         url: '/checkDirectory',
@@ -64,7 +55,6 @@ var RebindNode = React.createClass({
   else {
     this.props.bindings.forEach(function (ele, idx) {
       if (ele.key === code) {
-        console.log("has get requested failed yet?", code, ele);
         this.props.bindings[idx].path = path;
       }
     }, this);
